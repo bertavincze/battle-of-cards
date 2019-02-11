4c3f7935a37a4c505a4cb9195894a5b0faf828f2
AndreaSzoboszlai <szobandi@gmail.com>
2019-02-11 11:42:24 +0100

    player interface and player implement

--- src/main/java/com/codecool/api/Dealer.java
+++ src/main/java/com/codecool/api/Dealer.java
@@ -1,4 +1,17 @@
 package com.codecool.api;
 
+import java.util.List;
+import java.util.Random;
+
 public class Dealer {
+    private List<Card> deck;
+    private Card card;
+
+    public void randomChoose() {
+        Random r = new Random();
+    }
+
+    public void dealsTo(Player player) {
+        player.getHand().addCard(card);
+    }
 }
--- src/main/java/com/codecool/api/Deck.java
+++ src/main/java/com/codecool/api/Deck.java
@@ -6,6 +6,14 @@ public class Deck {
 
     private List<Card> cards;
 
+    public Deck() {
+        this.cards = ;
+    }
+
+    public List<Card> getDeck() {
+        return cards;
+    }
+
     public Card drawCard() {
         return null;
     }
