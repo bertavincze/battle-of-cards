109177be5ecd412a21ac6e9ebc0fece595fcd886
AndreaSzoboszlai <szobandi@gmail.com>
2019-02-11 13:57:22 +0100

    dealer updated

--- src/main/java/com/codecool/api/Dealer.java
+++ src/main/java/com/codecool/api/Dealer.java
@@ -1,17 +1,23 @@
 package com.codecool.api;
 
+import java.util.List;
+import java.util.Random;
+
 public class Dealer {
-    
+
     private Deck deck;
-    
+
     public Dealer(Deck deck){
         this.deck = deck;
     }
 
-    public void dealsTo(Player player) {
+    public void dealsTo(Player player, int numberOfCards) {
+
         while(deck.iterator().hasNext()){
-            player.getHand().addCard(deck.iterator().next());
-            deck.iterator().remove();
+            for (int i = 0; i < numberOfCards; i++) {
+                player.getHand().addCard(deck.iterator().next());
+                deck.iterator().remove();
+            }
         }
     }
 }
