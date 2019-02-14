65ab1e59b0c06c4f8823ec3280bcb05cce884fc3
Berta Vincze <berta.vincze@gmail.com>
2019-02-14 11:17:18 +0100

    trying java swing

--- src/main/java/com/codecool/guiprog/Main.java
+++ src/main/java/com/codecool/guiprog/Main.java
@@ -1,16 +1,33 @@
 package com.codecool.guiprog;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Main extends Application {
+public class Main {
+
+    private JPanel Panel1;
+    private JProgressBar button1;
+
 
     public static void main(String[] args) {
-        launch(args);
-    }
+        JFrame frame = new JFrame("Test");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setMinimumSize(new Dimension(800, 600));
+        String name = JOptionPane.showInputDialog(frame, "Test");
+        System.out.printf("The user's name is  '%s'.\n", name);
 
-    @Override
-    public void start(Stage primaryStage) {
+        /*
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showInputDialog(frame, "Enter your name");
+            }
+        });
+        */
 
+        frame.pack();
+        frame.setVisible(true);
     }
 }
